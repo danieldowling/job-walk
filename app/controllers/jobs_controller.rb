cf3ad3a98e1939cf6b41d457a74cef5d374a9e1a
Daniel Dowling <ddowling30@gmail.com>
2015-12-29 04:30:31 -0800

    can create, read, and update data for jobs

--- app/controllers/jobs_controller.rb
+++ app/controllers/jobs_controller.rb
@@ -1,2 +1,24 @@
 class JobsController < ApplicationController
+
+	def index
+		@jobs = Job.all
+	end
+
+	def show
+		@job = Job.find(params[:id])
+	end
+
+	def new
+		@job = Job.new
+	end
+
+	def create
+		@job = Job.new (params.require(:job).permit(:company_name, :job_title, :address_number, :street_name, :city, :zip_code, :job_description))
+
+		if @job.save
+			redirect_to jobs_path
+		else
+			render :new
+		end
+	end
 end
