ed8c072e122be2f7d950bb720a18de33094274a9
Daniel Dowling <ddowling30@gmail.com>
2016-01-04 22:45:43 -0800

    added links from index to show / show to edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,7 +3,7 @@ Rails.application.routes.draw do
   get "jobs/new" => "jobs#new", as: :new_job
   get "jobs/:id" => "jobs#show", as: :job
   post "jobs/" => "jobs#create"
-  get "jobs/:id/edit" => "jobs#edit", as: :edit_bean
+  get "jobs/:id/edit" => "jobs#edit", as: :edit_job
   patch "jobs/:id" => "jobs#update"
   delete "jobs/:id" => "jobs#destroy"
 end
