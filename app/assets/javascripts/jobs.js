4e3f3ae15e98314d264f7864d143b12218134ab0
Daniel Dowling <ddowling30@gmail.com>
2016-01-20 09:13:16 -0800

    changed jobs.coffee to jobs.js

---
+++ app/assets/javascripts/jobs.js
@@ -0,0 +1,11 @@
+function initialize () {
+	var mapProperties = {
+		center:new google.maps.LatLng(34.0129821,-118.495196),
+  		zoom: 17,
+  		mapTypeId: google.maps.MapTypeId.ROADMAP
+	};
+
+	var map=new google.maps.Map(document.getElementById("googleMap"), mapProperties);
+};
+
+google.maps.event.addDomListener(window, 'load', initialize);
